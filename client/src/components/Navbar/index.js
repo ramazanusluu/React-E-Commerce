734d173ad4bde4fec28ed80ec02591af8633787e
Ramazan <ruslu1720@gmail.com>
2022-06-05 13:41:48 +0300

    Chakra UI Kurulumu & Navbar'in Hazirlanmasi

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import { Button } from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Link to="/">Ξ-COMMΞRCΞ</Link>
+          </div>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">PRODUCT</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.right}>
+          <Link to="/signin">
+            <Button colorScheme="gray" variant="outline">
+              LOGIN
+            </Button>
+          </Link>
+          <Link to="/signup">
+            <Button colorScheme="gray" variant="outline">
+              REGISTER
+            </Button>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
