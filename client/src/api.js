62530525f1c24ace5b800abe79c9c0b76881f9e9
Ramazan <ruslu1720@gmail.com>
2022-06-06 14:08:08 +0300

    Kullanici kayit islemleri - 2 & Backend ile iletisim.

--- client/src/api.js
+++ client/src/api.js
@@ -13,3 +13,11 @@ export const fetchProduct = async (id) => {
   );
   return data;
 };
+
+export const fetchRegister = async (input) => {
+  const { data } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
+    input
+  );
+  return data;
+};
