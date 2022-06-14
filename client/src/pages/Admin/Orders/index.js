c4a61a7fe74800ed844612923684549d00f18ea8
Ramazan <ruslu1720@gmail.com>
2022-06-14 09:36:22 +0300

    Admin; Order Sayfası Eklendi

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,9 +1,54 @@
 import React from "react";
+import {
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+
+import { useQuery } from "react-query";
+import { fetchOrders } from "../../../api";
 
 function Orders() {
+  const { isLoading, isError, data, error } = useQuery(
+    "admin:orders",
+    fetchOrders
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error {error.message}</div>;
+  }
+  console.log(data);
   return (
     <div>
-      <h1>Orders Page</h1>
+      <Text fontSize="2xl" padding="5px">
+        Orders
+      </Text>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>User</Th>
+            <Th>Address</Th>
+            <Th isNumeric>Items</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item._id}>
+              <Td>{item.user.email}</Td>
+              <Td>{item.adress}</Td>
+              <Td isNumeric>{item.items.length}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </div>
   );
 }
